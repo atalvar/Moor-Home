df797d14e88c7ded73d4bc13cbba26678bd8f6c5
alaritalvar <107038278+alaritalvar@users.noreply.github.com>
2025-06-23 16:15:04 +0300

    fix again

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/Moor-Home/',  // <-- set this to your repo name
-  plugins: [react()],
-})
+/// <reference types="vite/client" />
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Moor-Home/',  // IMPORTANT: your repo name with leading and trailing slash
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
